d38e3c2e2f65e25dcb0b5018ac3460770624cab3
Jesús Di Bari <iijbari@gmail.com>
2017-06-08 07:24:21 +0000

    Add custom fonts

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,6 @@
+// $(document).on("ready", function(){
+// 	$(".clase").text("Texto normal.");
+// 	$("#id").html("<strong>Texto en negrita.</strong>");
+// 	var id = $("#valorDelId").attr("id"); //obtengo el valor del id.
+// 	$("#valorDelId").attr("class", "valorDeLaClase"); //asigno el valor de la clase.
+// });
