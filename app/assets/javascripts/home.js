28071129c7e00c05197c4e811f351d49c91b85c3
Jesús Di Bari <iijbari@gmail.com>
2017-06-09 05:33:00 +0000

    Add effect to right button and add toggle to contents

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -21,6 +21,11 @@ $(document).on("ready", function(){
       duration: 500,
       easing: 'easeOutQuint'
     });
+    $(".right-button").hide("drop", {
+      direction: "down",
+      duration: 500,
+      easing: 'easeOutQuint'
+    });
 		//$(this).css("border", "solid 1px");
 		$(this).animate({ borderColor: "white" });
 	});
@@ -50,8 +55,52 @@ $(document).on("ready", function(){
 		});
 	});
 	
+	$(".right-button").mouseenter(function(){
+		$(this).finish();
+		$(this).animate({
+      backgroundColor: "transparent",
+      color: "#d9534f",
+      borderColor: "#d9534f"
+    }, 500);
+    $(this).animate({ borderColor: "white", color: "white" });
+	});
+	$(".right-button").mouseleave(function(){
+		$(this).finish();
+		$(this).animate({
+			borderColor: "#d9534f",
+			color: "white",
+			backgroundColor: "#d9534f"
+		});
+	});
+	
+	$(".left-content").css("display", "none");
+	$(".left-button").click(function(){
+    $(".right-content").css("display", "none");
+    $(".left-content").toggle("slide", {
+      direction: "up",
+      duration: 500,
+      easing: 'easeOutQuint'
+    });
+  });
+  
+  $(".right-content").css("display", "none");
+	$(".right-button").click(function(){
+    $(".left-content").css("display", "none");
+    $(".right-content").toggle("slide", {
+      direction: "up",
+      duration: 500,
+      easing: 'easeOutQuint'
+    });
+  });
+	
+	$(".right-button").css("display", "none");
 	$(".right-box").mouseenter(function(){
 		$(this).finish();
+		$(".right-button").show("drop", {
+      direction: "up",
+      duration: 500,
+      easing: 'easeOutQuint'
+    });
 		$(".left-button").hide("drop", {
       direction: "down",
       duration: 500,
