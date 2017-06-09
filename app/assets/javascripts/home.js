5fb647a8ed5e48aca0451a1e83d94101f3c4fe55
Jesús Di Bari <iijbari@gmail.com>
2017-06-09 04:05:21 +0000

    Add images and button effects

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -13,8 +13,14 @@ $(document).on("ready", function(){
 		$(this).animate({ borderColor: "white" });
 	});
 	
+	$(".left-button").css("display", "none");
 	$(".left-box").mouseenter(function(){
 		$(this).finish();
+		$(".left-button").show("drop", {
+      direction: "up",
+      duration: 500,
+      easing: 'easeOutQuint'
+    });
 		//$(this).css("border", "solid 1px");
 		$(this).animate({ borderColor: "white" });
 	});
@@ -22,16 +28,41 @@ $(document).on("ready", function(){
 		$(this).finish();
 		$(this).animate({ borderColor: "transparent" });
 		$(this).animate({ borderLeftColor: "#990012" }, { queue: false });
+		$(this).animate({ borderRightColor: "#FFCCCC" }, { queue: false });
 		//$(this).css("border-left-color", "#990012");
+	});
+	
+	$(".left-button").mouseenter(function(){
+		$(this).finish();
+		$(this).animate({
+      backgroundColor: "transparent",
+      color: "#d9534f",
+      borderColor: "#d9534f"
+    }, 500);
+    $(this).animate({ borderColor: "white", color: "white" });
+	});
+	$(".left-button").mouseleave(function(){
+		$(this).finish();
+		$(this).animate({
+			borderColor: "#d9534f",
+			color: "white",
+			backgroundColor: "#d9534f"
+		});
 	});
 	
 	$(".right-box").mouseenter(function(){
 		$(this).finish();
+		$(".left-button").hide("drop", {
+      direction: "down",
+      duration: 500,
+      easing: 'easeOutQuint'
+    });
 		$(this).animate({ borderColor: "white" });
 	});
 	$(".right-box").mouseleave(function(){
 		$(this).finish();
 		$(this).animate({ borderColor: "transparent" });
 		$(this).animate({ borderRightColor: "#990012" }, { queue: false });
+		$(this).animate({ borderLeftColor: "#FFCCCC" }, { queue: false });
 	});
 });
